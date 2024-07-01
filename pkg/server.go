26b13b16af838c8957c4b27c85df333e49a42578
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2024-07-01 08:09:39 +0200

    added some comments

--- pkg/server.go
+++ pkg/server.go
@@ -2,34 +2,41 @@ package pkg
 
 import "fmt"
 
+// Server contains the server's setup values.
 type Server struct {
 	Name    string
 	Address string
 	Port    int
 }
 
+// ServerOpts groups all valid options for the server together.
 type ServerOpts struct{}
 
+// WithName adds an options to define the name of the server.
 func (ServerOpts) WithName(name string) ServerOptFn {
 	return func(s *Server) {
 		s.Name = name
 	}
 }
 
+// WithAddress adds the address of the server.
 func (ServerOpts) WithAddress(address string) ServerOptFn {
 	return func(s *Server) {
 		s.Address = address
 	}
 }
 
+// WithPort defines the port on which the server should run.
 func (ServerOpts) WithPort(port int) ServerOptFn {
 	return func(s *Server) {
 		s.Port = port
 	}
 }
 
+// ServerOptFn defines the function type to configure a server.
 type ServerOptFn func(*Server)
 
+// NewServer creates a new configured server.
 func NewServer(opts ...ServerOptFn) *Server {
 	s := &Server{}
 
@@ -40,6 +47,7 @@ func NewServer(opts ...ServerOptFn) *Server {
 	return s
 }
 
+// Start starts the server and outputs the configured options.
 func (s *Server) Start() {
 	fmt.Printf("started server with name: %s; address: %s; port: %d\n", s.Name, s.Address, s.Port)
 }
